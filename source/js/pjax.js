6a85a2e5ad021e0e325ccdd27a261243b025479c
ruby woo <i520gfy@gmail.com>
2020-04-21 20:28:51 +0800

    added screen scrolling after the end of pjax

--- source/js/pjax.js
+++ source/js/pjax.js
@@ -13,4 +13,7 @@ $(document).on('pjax:complete', function() {
     document.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightBlock(block);
     });
+    $('.container').velocity('scroll', {
+        offset: $('#header').height() - 60
+    });
 });
