a1c8cfed224a061bfa835d1ccb0c53b2d6ac1bc3
ruby woo <i520gfy@gmail.com>
2020-04-24 22:17:28 +0800

    resolved https://github.com/feiyangbeyond/halo-theme-sagiri/issues/6

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,4 @@
-const sagiri =  {
+const sagiri = {
     // 目录
     postToc: function () {
         var headerEl = 'h1,h2,h3,h4,h5,h6';
@@ -14,14 +14,14 @@ const sagiri =  {
         });
     },
     // 点击
-    scrolldown: function(){
+    scrolldown: function () {
         //滚动到第二屏
-        $('.scrolldown_b').on('click', function() {
+        $('.scrolldown_b').on('click', function () {
             $('.container').velocity('scroll', {
                 offset: $('#header').height() - 60
             });
         });
-        $('.scrolldown_s').on('click', function() {
+        $('.scrolldown_s').on('click', function () {
             $('.container').velocity('scroll', {
                 offset: $('#header').height()
             });
@@ -29,7 +29,7 @@ const sagiri =  {
     },
 
     // 悬浮
-    affix: function(){
+    affix: function () {
         $('.sidebar-inner').affix({
             offset: {
                 top: 700,
@@ -38,8 +38,19 @@ const sagiri =  {
         })
     },
 
+    // 站点运行时间
+    blogRanTime: function (now, start) {
+        var n = new Date(start);
+        now.setTime(now.getTime() + 250), days = (now - n) / 1e3 / 60 / 60 / 24, dnum = Math.floor(days), hours = (now - n) /
+            1e3 / 60 / 60 - 24 * dnum, hnum = Math.floor(hours), 1 == String(hnum).length && (hnum = "0" + hnum), minutes =
+            (now - n) / 1e3 / 60 - 1440 * dnum - 60 * hnum, mnum = Math.floor(minutes), 1 == String(mnum).length && (mnum =
+            "0" + mnum), seconds = (now - n) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum, snum = Math.round(seconds),
+        1 == String(snum).length && (snum = "0" + snum), document.getElementById("timeDate").innerHTML = "站点已萌萌哒运行 " +
+            dnum + " 天 ", document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒"
+    },
+
     // 歌词
-    operation_aplayer_lrc: function(){
+    operation_aplayer_lrc: function () {
         var scrollTop = $(window).scrollTop();
         var scrollHeight = $(document).height();
         var windowHeight = $(window).height();
@@ -51,19 +62,19 @@ const sagiri =  {
     },
 
     // 图片懒加载
-    lazyload: function(className){
+    lazyload: function (className) {
         $(className).lazyload({
             effect: "fadeIn",
             placeholder: "https://i.loli.net/2020/04/24/4YqE3JN2zmGHujC.gif"
         });
     },
 
-    fancybox: function(){
+    fancybox: function () {
         $(".fancybox").fancybox();
     }
 }
 
-$(function() {
+$(function () {
     sagiri.scrolldown();
     hljs.initHighlightingOnLoad();
     sagiri.operation_aplayer_lrc();
